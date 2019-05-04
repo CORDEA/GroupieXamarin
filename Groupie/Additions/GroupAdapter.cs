23fbb07b477ec9e3ddcb7b72428ead3b48b261d9
Yoshihiro Tanaka <contact@cordea.jp>
2019-05-04 12:40:51 +0900

    override required methods

---
+++ Groupie/Additions/GroupAdapter.cs
@@ -0,0 +1,18 @@
+﻿using Android.Support.V7.Widget;
+using Android.Views;
+
+namespace Xwray.Groupie
+{
+    public partial class GroupAdapter
+    {
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            // Do nothing here because original source does nothing.
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            return (RecyclerView.ViewHolder) InternalCreateViewHolder(parent, viewType);
+        }
+    }
+}
