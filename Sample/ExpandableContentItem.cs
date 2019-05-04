2bb4413777c62ca67e29ff43c04a1f7501bf6b85
Yoshihiro Tanaka <contact@cordea.jp>
2019-05-04 19:01:07 +0900

    add content item

---
+++ Sample/ExpandableContentItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Android.Widget;
+using Xwray.Groupie;
+using Object = Java.Lang.Object;
+
+namespace Sample
+{
+    public class ExpandableContentItem : Item
+    {
+        private readonly string _description;
+
+        public ExpandableContentItem(string description)
+        {
+            _description = description;
+        }
+
+        public override int Layout => Resource.Layout.expandable_content_item;
+
+        public override void Bind(Object p0, int p1)
+        {
+            var viewHolder = (ViewHolder) p0;
+            var description = viewHolder.Root.FindViewById<TextView>(Resource.Id.description);
+            description.Text = _description;
+        }
+    }
+}
