117154161eef693dfd83087099e60563fa1b172e
Yoshihiro Tanaka <contact@cordea.jp>
2019-05-04 17:35:59 +0900

    define item

---
+++ Sample/ListItem.cs
@@ -0,0 +1,25 @@
+﻿using Android.Widget;
+using Java.Lang;
+using Xwray.Groupie;
+
+namespace Sample
+{
+    public class ListItem : Item
+    {
+        private string _title;
+
+        public ListItem(string title)
+        {
+            _title = title;
+        }
+
+        public override int Layout => Resource.Layout.list_item;
+
+        public override void Bind(Object p0, int p1)
+        {
+            var viewHolder = (ViewHolder) p0;
+            var title = viewHolder.Root.FindViewById<TextView>(Resource.Id.title);
+            title.Text = _title;
+        }
+    }
+}
