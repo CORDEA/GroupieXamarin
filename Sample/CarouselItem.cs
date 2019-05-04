c3606c5dd71af7127d6a11b680f0394fe19990c5
Yoshihiro Tanaka <contact@cordea.jp>
2019-05-04 21:22:22 +0900

    add carousel item

---
+++ Sample/CarouselItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Android.Support.V7.Widget;
+using Android.Widget;
+using Java.Lang;
+using Xwray.Groupie;
+
+namespace Sample
+{
+    public class CarouselItem : Item
+    {
+        private readonly GroupAdapter _groupAdapter = new GroupAdapter();
+        private readonly string _title;
+        private readonly IList<IGroup> _childItems;
+
+        public CarouselItem(string title, IList<IGroup> childItems)
+        {
+            _title = title;
+            _childItems = childItems;
+        }
+
+        public override int Layout => Resource.Layout.carousel_item;
+
+        public override void Bind(Object p0, int p1)
+        {
+            var viewHolder = (ViewHolder) p0;
+            var title = viewHolder.Root.FindViewById<TextView>(Resource.Id.title);
+            title.Text = _title;
+            var recyclerView = viewHolder.Root.FindViewById<RecyclerView>(Resource.Id.recycler_view);
+            recyclerView.SetLayoutManager(new LinearLayoutManager(viewHolder.Root.Context, LinearLayoutManager.Horizontal, false));
+            recyclerView.SetAdapter(_groupAdapter);
+            _groupAdapter.Clear();
+            _groupAdapter.AddAll(_childItems);
+        }
+    }
+}
