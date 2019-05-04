0e4cce7e82e57475d85434a6d172a5d304cc2cf2
Yoshihiro Tanaka <contact@cordea.jp>
2019-05-04 21:22:03 +0900

    add carousel child item

---
+++ Sample/CarouselChildItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Android.Widget;
+using Xwray.Groupie;
+using Object = Java.Lang.Object;
+
+namespace Sample
+{
+    public class CarouselChildItem : Item
+    {
+        private readonly string _title;
+
+        public CarouselChildItem(string title)
+        {
+            _title = title;
+        }
+
+        public override int Layout => Resource.Layout.carousel_child_item;
+
+        public override void Bind(Object p0, int p1)
+        {
+            var viewHolder = (ViewHolder) p0;
+            var title = viewHolder.Root.FindViewById<TextView>(Resource.Id.title);
+            title.Text = _title;
+        }
+    }
+}
