bf1d7dd95abd289169c28c5d0b7a50a123670af2
Yoshihiro Tanaka <contact@cordea.jp>
2019-05-04 13:04:47 +0900

    remove menu

--- Sample/MainActivity.cs
+++ Sample/MainActivity.cs
@@ -26,29 +26,13 @@ namespace Sample
             fab.Click += FabOnClick;
         }
 
-        public override bool OnCreateOptionsMenu(IMenu menu)
-        {
-            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
-            return true;
-        }
-
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            int id = item.ItemId;
-            if (id == Resource.Id.action_settings)
-            {
-                return true;
-            }
-
-            return base.OnOptionsItemSelected(item);
-        }
-
         private void FabOnClick(object sender, EventArgs eventArgs)
         {
-            View view = (View)sender;
+            View view = (View) sender;
             Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+                .SetAction("Action", (Android.Views.View.IOnClickListener) null).Show();
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -57,4 +41,3 @@ namespace Sample
         }
     }
 }
-
